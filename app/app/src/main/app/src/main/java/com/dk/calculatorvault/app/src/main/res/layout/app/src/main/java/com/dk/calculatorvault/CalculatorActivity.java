134d57558c96823fa66f501e52ffb0809713aa85
sanjeev7579 <sanjeevverms47@gmail.com>
2025-07-31 12:42:02 +0000

    Added CalculatorActivity logic

--- app/app/src/main/app/src/main/java/com/dk/calculatorvault/app/src/main/res/layout/app/src/main/java/com/dk/calculatorvault/CalculatorActivity.java
+++ app/app/src/main/app/src/main/java/com/dk/calculatorvault/app/src/main/res/layout/app/src/main/java/com/dk/calculatorvault/CalculatorActivity.java
@@ -1,54 +1,32 @@
 package com.dk.calculatorvault;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
+import android.widget.EditText;
+import androidx.appcompat.app.AppCompatActivity;
 
-public class CalculatorActivity extends Activity {
+public class CalculatorActivity extends AppCompatActivity {
 
-    private String input = "";
-    private final String secretCode = "1234";
-    private TextView display;
+    EditText passwordField;
+    Button loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calculator);
 
-        display = findViewById(R.id.textViewDisplay);
+        passwordField = findViewById(R.id.passwordEditText);
+        loginButton = findViewById(R.id.loginButton);
 
-        int[] buttonIds = {
-            R.id.button0, R.id.button1, R.id.button2, R.id.button3,
-            R.id.button4, R.id.button5, R.id.button6, R.id.button7,
-            R.id.button8, R.id.button9
-        };
-
-        View.OnClickListener listener = new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Button b = (Button) v;
-                input += b.getText().toString();
-                display.setText(input);
-            }
-        };
-
-        for (int id : buttonIds) {
-            Button btn = findViewById(id);
-            btn.setOnClickListener(listener);
-        }
-
-        Button equalButton = findViewById(R.id.buttonEqual);
-        equalButton.setOnClickListener(v -> {
-            if (input.equals(secretCode)) {
-                startActivity(new Intent(CalculatorActivity.this, VaultActivity.class));
-                input = "";
-                display.setText("");
+        loginButton.setOnClickListener(v -> {
+            String password = passwordField.getText().toString();
+            if (password.equals("1234")) {
+                // Correct password, go to vault
+                Intent intent = new Intent(CalculatorActivity.this, VaultActivity.class);
+                startActivity(intent);
             } else {
-                display.setText("Wrong PIN");
-                input = "";
+                passwordField.setError("Wrong Password");
             }
         });
     }
